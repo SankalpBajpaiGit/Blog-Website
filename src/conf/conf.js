5408476ef04fe0af4f813eb4f4c916ab3a700448
SankalpBajpaiGit <154622097+SankalpBajpaiGit@users.noreply.github.com>
2025-03-17 15:00:07 +0530

    Update conf.js

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,9 +1,9 @@
 const conf = {
-    appwriteUrl: "https://cloud.appwrite.io/v1",
-    appwriteProjectId: "679bd200000381aea990",
-    appwriteDatabaseId: "679bd2db00243679071f",
-    appwriteCollectionId: "679bd2fd001d136b9733",
-    appwriteBucketId: "679bd4600012221a3175"
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 }
 
 
