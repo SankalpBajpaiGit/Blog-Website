248c7bf87d325e48e030df8a72da3bb4773f88be
Sankalp Bajpai <154622097+SankalpBajpaiGit@users.noreply.github.com>
2025-03-30 16:52:02 +0530

    Update LogoutBtn.jsx

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -11,7 +11,12 @@ function LogoutBtn() {
         })
     }
   return (
-    <button className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full' onClick={logoutHandler}>Logout</button>
+    <button 
+      className="inline-block px-6 py-2 bg-blue-500 text-yellow-100 border-2 border-yellow-300 rounded-full transition-all duration-200 hover:bg-yellow-300 hover:text-blue-900 shadow-md font-[Comic Sans MS]"
+      onClick={logoutHandler}
+    >
+      Logout
+    </button>
   )
 }
 
